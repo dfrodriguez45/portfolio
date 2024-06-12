4cd2bf0f43acedb3b6a903b49a7568fff0ad6db3
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-12 13:35:57 -0600

    chore: Update all portfolio

--- src/components/StarsBackground/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function StarsBackground() {
-  return (
-    <div className="absolute inset-0 -z-0 overflow-hidden">
-      <div id="stars" />
-      <div id="stars2" />
-      <div id="stars3" />
-    </div>
-  );
-}
