38861403affd217fa69bcec3e8e3871cac854926
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-15 17:24:07 -0600

    chore: Update experience section in App component

--- src/App.jsx
+++ src/App.jsx
@@ -215,34 +215,114 @@ function App() {
         <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800">
           <h2 className="text-center text-2xl font-bold">Experiencia</h2>
           <ul className="w-full border-l">
-            <li className="relative flex w-full flex-col items-center justify-between gap-4 rounded-lg p-6 md:flex-row">
-              <div>
-                <div className="absolute -left-1.5 top-7 size-3 rounded-full bg-gray-400 md:top-14" />
-                <h3 className="text-lg font-bold">Desarrollador Fullstack</h3>
-                <p className="font-medium italic">Perla Technology</p>
-                <p className="font-light">jul. 2022 - dic. 2023</p>
+            <li className="relative w-full p-6">
+              <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                <div>
+                  <div className="absolute -left-1.5 top-7 size-3 rounded-full bg-gray-400 md:top-14" />
+                  <h3 className="text-lg font-bold">Desarrollador Fullstack</h3>
+                  <p className="font-medium italic">Perla Technology</p>
+                  <p className="font-light">jul. 2022 - dic. 2023</p>
+                </div>
+                <img
+                  src="experience/perla-technology.png"
+                  alt="Perla Technology"
+                  className="w-3/4 md:w-1/3"
+                />
               </div>
-              <img
-                src="experience/perla-technology.png"
-                alt="Perla Technology"
-                className="w-3/4 md:w-1/3"
-              />
+              <ul className="mt-4 list-inside list-disc">
+                <li className="list-item">
+                  Participar en reuniones para discutir requisitos y progreso
+                  del proyecto.
+                </li>
+                <li className="list-item">
+                  Configurar Firebase para autenticación, base de datos y
+                  funciones en la nube.
+                </li>
+                <li className="list-item">
+                  Convertir la interfaz móvil a una aplicación web usando React.
+                </li>
+                <li className="list-item">
+                  Integrar la aplicación web con Firebase para autenticación y
+                  base de datos.
+                </li>
+                <li className="list-item">
+                  Identificar y corregir errores durante el desarrollo y las
+                  pruebas.
+                </li>
+                <li className="list-item">
+                  Desplegar la aplicación web en el servicio de hosting de
+                  Firebase.
+                </li>
+                <li className="list-item">
+                  Monitorear el rendimiento, corregir errores y mejorar la
+                  aplicación.
+                </li>
+                <li className="list-item">
+                  Escribir documentación para facilitar la comprensión y
+                  mantenimiento del código.
+                </li>
+              </ul>
             </li>
-            <li className="relative flex w-full flex-col items-center justify-between gap-4 rounded-lg p-6 md:flex-row">
-              <div>
-                <div className="absolute -left-1.5 top-7 size-3 rounded-full bg-gray-400 md:top-14" />
-                <h3 className="text-lg font-bold">Ingeniero en Software</h3>
-                <p className="font-medium italic">SoftwareOne Colombia</p>
-                <p className="font-light">abr. 2023 - oct. 2023</p>
+            <li className="relative w-full p-6">
+              <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                <div>
+                  <div className="absolute -left-1.5 top-7 size-3 rounded-full bg-gray-400 md:top-14" />
+                  <h3 className="text-lg font-bold">
+                    Aprendiz Desarrollador de Software
+                  </h3>
+                  <p className="font-medium italic">SoftwareOne Colombia</p>
+                  <p className="font-light">abr. 2023 - oct. 2023</p>
+                </div>
+                <img
+                  src="experience/softwareone.png"
+                  alt="SoftwareOne Colombia"
+                  className="w-3/4 md:w-1/3"
+                />
               </div>
-              <img
-                src="experience/softwareone.png"
-                alt="SoftwareOne Colombia"
-                className="w-3/4 md:w-1/3"
-              />
+              <ul className="mt-4 list-inside list-disc">
+                <li className="list-item">
+                  Participar en reuniones diarias de Scrum (Daily) para
+                  coordinar tareas.
+                </li>
+                <li className="list-item">
+                  Revisar y comprender los requisitos del cliente para los
+                  nuevos componentes.
+                </li>
+                <li className="list-item">
+                  Diseñar componentes React basados en los requisitos del
+                  cliente.
+                </li>
+                <li className="list-item">
+                  Escribir código de componentes React con reutilización.
+                </li>
+                <li className="list-item">
+                  Realizar pruebas unitarias y de integración para los nuevos
+                  componentes.
+                </li>
+                <li className="list-item">
+                  Integrar los nuevos componentes en la aplicación existente.
+                </li>
+                <li className="list-item">
+                  Revisar el código con el equipo para asegurar calidad y
+                  cumplimiento de estándares.
+                </li>
+                <li className="list-item">
+                  Desplegar y probar los componentes en un entorno de
+                  desarrollo.
+                </li>
+                <li className="list-item">
+                  Participar en reuniones de Sprint Review para presentar los
+                  avances al cliente y obtener feedback.
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
+        <img
+          src="footer.png"
+          alt="Zorro durmiendo"
+          className="col-span-2 mx-auto w-1/2"
+        />
       </div>
     </div>
   );
