15a3bf61f015a05f2b65cdd61daf740770c4a9e2
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-14 19:17:40 -0600

    chore: Update npm dependencies and apply new styles to index.css and App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { FaExternalLinkAlt, FaGithub, FaLinkedin } from "react-icons/fa";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 function App() {
   return (
-    <div className="flex flex-col gap-6 p-4 max-w-7xl mx-auto bg-gray-900 text-white">
-      <h1 className="text-4xl text-center sm:text-6xl font-bold bg-clip-text bg-gradient-to-t from-orange-500 to-orange-300 text-transparent p-2">
+    <div className="container mx-auto flex max-w-7xl flex-col gap-6 p-4">
+      <h1 className="bg-gradient-to-t from-orange-500 to-orange-300 bg-clip-text p-2 text-center text-4xl font-bold text-transparent sm:text-6xl">
         Daniel Rodriguez
       </h1>
-      <div className="flex flex-row gap-4 items-center justify-center">
+      {/* Social networks */}
+      <div className="flex flex-row items-center justify-center gap-4">
         <a
-          className="flex items-center justify-center bg-white text-orange-400 rounded-full p-2"
+          className="flex items-center justify-center rounded-full border p-2 text-orange-400"
           href="https://www.linkedin.com/in/dfrodriguez45/"
           target="_blank"
           rel="noopener noreferrer"
@@ -16,7 +22,7 @@ function App() {
           <FaLinkedin size={28} />
         </a>
         <a
-          className="flex items-center justify-center bg-white text-orange-400 rounded-full p-2"
+          className="flex items-center justify-center rounded-full border p-2 text-orange-400"
           href="https://github.com/dfrodriguez45"
           target="_blank"
           rel="noopener noreferrer"
@@ -24,9 +30,10 @@ function App() {
           <FaGithub size={28} />
         </a>
       </div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg p-6 text-slate-800">
-          <h2 className="text-2xl font-bold text-center">Sobre mí</h2>
+      <div className="grid grid-cols-2 gap-6">
+        {/* About me */}
+        <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800 lg:col-span-1">
+          <h2 className="text-center text-2xl font-bold">Sobre mí</h2>
           <p className="text-lg">
             Soy Ingeniero en Software con una sólida formación en tecnologías de
             información y especialización en el desarrollo web. Con experiencia
@@ -34,48 +41,66 @@ function App() {
             innovadoras, disfruto trabajando en proyectos que me desafían a
             adoptar nuevas tecnologías y expandir mis habilidades.
           </p>
-          <h2 className="text-lg font-bold text-center">Habilidades</h2>
-          <div className="flex flex-row flex-wrap gap-6 items-center justify-center">
+        </div>
+        {/* Skills */}
+        <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800 lg:col-span-1">
+          <h2 className="text-center text-2xl font-bold">Habilidades</h2>
+          <div className="flex flex-row flex-wrap items-center justify-center gap-6">
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
             <img
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-              className="size-10"
+              className="size-10 lg:size-20"
             />
           </div>
         </div>
-        <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg p-6 text-slate-800">
-          <h2 className="text-2xl font-bold text-center">Proyectos</h2>
-          <ul className="flex flex-col gap-4">
-            <li className="relative overflow-hidden rounded-lg">
+        {/* Projects */}
+        <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800">
+          <h2 className="text-center text-2xl font-bold">Proyectos</h2>
+          {/* Swiper */}
+          <Swiper
+            modules={[Pagination, Autoplay]}
+            direction={"horizontal"}
+            pagination={{
+              clickable: true,
+              dynamicBullets: true,
+            }}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: true,
+            }}
+            className="w-full overflow-hidden rounded-lg border pb-6"
+            slidesPerView={1}
+          >
+            <SwiperSlide className="relative">
               <img
                 src="/projects/api-client-fortnite.jpeg"
                 alt="api-client-fortnite"
-                className="w-full h-auto object-cover"
+                className="min-h-96 w-full object-cover"
               />
-              <div className="absolute flex items-center justify-between gap-2 bg-black/70 text-white font-bold bottom-10 md:bottom-14 w-full p-4">
+              <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
                 <span className="md:text-lg">Tienda de Fortnite</span>
                 <div className="flex gap-2">
                   <a
@@ -94,7 +119,7 @@ function App() {
                   </a>
                 </div>
               </div>
-              <div className="flex flex-row gap-2 bg-gray-800 p-2 items-center justify-center">
+              <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
                 <img
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
                   className="size-6 md:size-10"
@@ -108,14 +133,14 @@ function App() {
                   className="size-6 md:size-10"
                 />
               </div>
-            </li>
-            <li className="relative overflow-hidden rounded-lg">
+            </SwiperSlide>
+            <SwiperSlide className="relative">
               <img
                 src="/projects/centinela-universal-web.jpeg"
                 alt="centinela-universal-web"
-                className="w-full h-auto object-cover"
+                className="min-h-96 w-full object-cover"
               />
-              <div className="absolute flex items-center justify-between gap-2 bg-black/70 text-white font-bold bottom-10 md:bottom-14 w-full p-4">
+              <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
                 <span className="md:text-lg">Centinela Universal Web</span>
                 <div className="flex gap-2">
                   <a
@@ -127,7 +152,7 @@ function App() {
                   </a>
                 </div>
               </div>
-              <div className="flex flex-row gap-2 bg-gray-800 p-2 items-center justify-center">
+              <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
                 <img
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
                   className="size-6 md:size-10"
@@ -141,14 +166,14 @@ function App() {
                   className="size-6 md:size-10"
                 />
               </div>
-            </li>
-            <li className="relative overflow-hidden rounded-lg">
+            </SwiperSlide>
+            <SwiperSlide className="relative">
               <img
                 src="/projects/adeline-site.jpeg"
                 alt="centinela-universal-web"
-                className="w-full h-auto object-cover"
+                className="min-h-96 w-full object-cover"
               />
-              <div className="absolute flex items-center justify-between gap-2 bg-black/70 text-white font-bold bottom-10 md:bottom-14 w-full p-4">
+              <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
                 <span className="md:text-lg">Adeline Salón de Belleza</span>
                 <div className="flex gap-2">
                   <a
@@ -167,7 +192,7 @@ function App() {
                   </a>
                 </div>
               </div>
-              <div className="flex flex-row gap-2 bg-gray-800 p-2 items-center justify-center">
+              <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
                 <img
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg"
                   className="size-6 md:size-10"
@@ -181,10 +206,11 @@ function App() {
                   className="size-6 md:size-10"
                 />
               </div>
-            </li>
-          </ul>
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <div className="flex flex-col gap-4 bg-white rounded-lg shadow-lg p-6 text-slate-800">
+        {/* Experience */}
+        <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800">
           <h2 className="text-2xl font-bold text-center">Experiencia</h2>
         </div>
       </div>
