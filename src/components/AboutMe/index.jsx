7eba0dd35126f59171f073578958ab5d27d4fd9d
Daniel Rodriguez <60363007+dfrodriguez45@users.noreply.github.com>
2024-06-16 18:59:05 +0000

    chore: Update AboutMe component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,7 +3,7 @@ function AboutMe() {
     <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800 lg:col-span-1">
       <h2 className="text-center text-2xl font-bold">Sobre mí</h2>
       <p className="text-lg">
-        Soy Ingeniero en Software con una sólida formación y especialización en
+        Soy Desarrollador de Software con una sólida formación y especialización en
         el desarrollo web. Con experiencia en metodologías ágiles y un enfoque
         en el desarrollo de soluciones innovadoras, disfruto trabajando en
         proyectos que me desafían a adoptar nuevas tecnologías y expandir mis
