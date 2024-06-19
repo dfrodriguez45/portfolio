2c47b267b1ef7dedd9b4779ec050f41b2fd4a366
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-19 12:40:52 -0600

    feat: Add alt attributes to Skills component images

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -6,30 +6,37 @@ function Skills() {
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
           className="size-10 lg:size-20"
+          alt="HTML5 icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
           className="size-10 lg:size-20"
+          alt="CSS3 icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
           className="size-10 lg:size-20"
+          alt="JavaScript icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
           className="size-10 lg:size-20"
+          alt="React icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
           className="size-10 lg:size-20"
+          alt="Node.js icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
           className="size-10 lg:size-20"
+          alt="Firebase icon"
         />
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
           className="size-10 lg:size-20"
+          alt="MySQL icon"
         />
       </div>
     </div>
