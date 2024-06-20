83f8940a831a99d3c9f61175c478c559292f72d1
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-20 17:53:17 -0600

    feat: Update AboutMe component with revised text

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,10 +3,10 @@ function AboutMe() {
     <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800 lg:col-span-1">
       <h2 className="text-center text-2xl font-bold">Sobre mí</h2>
       <p className="text-lg">
-        Soy Desarrollador de Software con una sólida formación y especialización en
-        el desarrollo web. Con experiencia en metodologías ágiles y un enfoque
-        en el desarrollo de soluciones innovadoras, disfruto trabajando en
-        proyectos que me desafían a adoptar nuevas tecnologías y expandir mis
+        Soy Desarrollador de Software con una sólida formación y especialización
+        en el desarrollo web. Con experiencia en metodologías ágiles y un
+        enfoque en el desarrollo de soluciones innovadoras, disfruto trabajando
+        en proyectos que me desafían a adoptar nuevas tecnologías y expandir mis
         habilidades. Mi capacidad para trabajar con equipos multifuncionales y
         aplicar metodologías ágiles ha sido clave para entregar soluciones
         eficientes y de alta calidad.
