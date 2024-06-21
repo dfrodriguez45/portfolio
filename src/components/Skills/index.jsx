020d805a95b61b15107b67e0b6fb42ae99a62c2f
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-20 18:09:36 -0600

    refactor: Update Skills component to use dynamic data for skills icons

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,50 +1,21 @@
+import { skills } from "../../constants/skills";
+
 function Skills() {
   return (
     <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800 lg:col-span-1">
       <h2 className="text-center text-2xl font-bold">Habilidades</h2>
       <div className="flex flex-row flex-wrap items-center justify-center gap-6">
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-          className="size-10 lg:size-20"
-          alt="HTML5 icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-          className="size-10 lg:size-20"
-          alt="CSS3 icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-          className="size-10 lg:size-20"
-          alt="JavaScript icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          className="size-10 lg:size-20"
-          alt="React icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-          className="size-10 lg:size-20"
-          alt="Node.js icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
-          className="size-10 lg:size-20"
-          alt="Firebase icon"
-          loading="lazy"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-          className="size-10 lg:size-20"
-          alt="MySQL icon"
-          loading="lazy"
-        />
+        {skills.map((skill, index) => {
+          return (
+            <img
+              key={index}
+              src={skill.icon}
+              className="size-10 lg:size-20"
+              alt={skill.alt}
+              loading="lazy"
+            />
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/constants/skills.js
@@ -0,0 +1,30 @@
+export const skills = [
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    alt: "HTML5 icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    alt: "CSS3 icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    alt: "JavaScript icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    alt: "React icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    alt: "Node.js icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+    alt: "Firebase icon",
+  },
+  {
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    alt: "MySQL icon",
+  },
+];
