41a6169f77776a7bba0aea0a4d29ffe390b621e2
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-20 18:22:01 -0600

    feat: Add projects data to display a list of projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,9 +1,10 @@
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+
+import { projects } from "../../constants/projects";
 
 function Projects() {
   return (
@@ -24,146 +25,50 @@ function Projects() {
         className="w-full overflow-hidden rounded-lg border pb-6"
         slidesPerView={1}
       >
-        <SwiperSlide className="relative">
-          <img
-            src="/projects/api-client-fortnite.webp"
-            alt="api-client-fortnite"
-            className="min-h-96 w-full object-cover"
-            width={1733}
-            height={863}
-            loading="lazy"
-          />
-          <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
-            <span className="md:text-lg">Tienda de Fortnite</span>
-            <div className="flex gap-2">
-              <a
-                href="https://fortnite-store.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaExternalLinkAlt size={24} />
-              </a>
-              <a
-                href="https://github.com/dfrodriguez45/fortnite-store"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaGithub size={24} />
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
-              className="size-6 md:size-10"
-              alt="Vite.js icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-              className="size-6 md:size-10"
-              alt="React icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-              className="size-6 md:size-10"
-              alt="Tailwind CSS icon"
-              loading="lazy"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="relative">
-          <img
-            src="/projects/centinela-universal-web.webp"
-            alt="centinela-universal-web"
-            className="min-h-96 w-full object-cover"
-            width={1733}
-            height={863}
-            loading="lazy"
-          />
-          <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
-            <span className="md:text-lg">Centinela Universal Web</span>
-            <div className="flex gap-2">
-              <a
-                href="https://centinela-universal-acff4.web.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaExternalLinkAlt size={24} />
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
-              className="size-6 md:size-10"
-              alt="Vite.js icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-              className="size-6 md:size-10"
-              alt="React icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-              className="size-6 md:size-10"
-              alt="Tailwind CSS icon"
-              loading="lazy"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="relative">
-          <img
-            src="/projects/adeline-site.webp"
-            alt="centinela-universal-web"
-            className="min-h-96 w-full object-cover"
-            width={1733}
-            height={863}
-            loading="lazy"
-          />
-          <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
-            <span className="md:text-lg">Adeline Salón de Belleza</span>
-            <div className="flex gap-2">
-              <a
-                href="https://adeline-site.web.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaExternalLinkAlt size={24} />
-              </a>
-              <a
-                href="https://github.com/dfrodriguez45/adeline-site"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaGithub size={24} />
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg"
-              className="size-6 md:size-10"
-              alt="Astro icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-              className="size-6 md:size-10"
-              alt="React icon"
-              loading="lazy"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-              className="size-6 md:size-10"
-              alt="Tailwind CSS icon"
-              loading="lazy"
-            />
-          </div>
-        </SwiperSlide>
+        {projects.map((project, index) => {
+          return (
+            <SwiperSlide key={index} className="relative">
+              <img
+                src={project.img.src}
+                alt={project.img.alt}
+                className="min-h-96 w-full object-cover"
+                width={project.img.width}
+                height={project.img.height}
+                loading="lazy"
+              />
+              <div className="absolute bottom-10 flex w-full items-center justify-between gap-2 bg-black/70 p-4 font-bold text-white md:bottom-14">
+                <span className="md:text-lg">{project.title}</span>
+                <div className="flex gap-2">
+                  {project.links.map((link, index) => {
+                    return (
+                      <a
+                        key={index}
+                        href={link.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <link.icon size={24} />
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-center gap-2 bg-gray-800 p-2">
+                {project.technologies.map((technology, index) => {
+                  return (
+                    <img
+                      key={index}
+                      src={technology.icon}
+                      className="size-6 md:size-10"
+                      alt={technology.alt}
+                      loading="lazy"
+                    />
+                  );
+                })}
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
