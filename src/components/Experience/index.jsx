edfefe2572055135b32e449585df9b6e721b2bd8
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-21 19:04:06 -0600

    feat: Update language selection in LanguagesProvider and use it in Skills, Experience, and Projects components

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+
 import { experiences } from "../../constants/experiences";
+import { LanguagesContext } from "../../contexts/LanguagesContext";
 
 function Experience() {
+  const { language } = useContext(LanguagesContext);
+
   return (
     <div className="col-span-2 flex flex-col gap-4 rounded-lg bg-white p-6 text-slate-800">
-      <h2 className="text-center text-2xl font-bold">Experiencia</h2>
+      <h2 className="text-center text-2xl font-bold">
+        {experiences[language].title}
+      </h2>
       <ul className="w-full border-l">
-        {experiences.map((experience, index) => {
+        {experiences[language].list.map((experience, index) => {
           return (
             <li key={index} className="relative w-full p-6">
               <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
