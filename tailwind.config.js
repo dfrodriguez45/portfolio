69dfbee3b2d56a1d64d81bc0ccedc3beb8ebf717
Daniel Rodriguez <dfrodriguez_45@outlook.com>
2024-06-23 13:26:50 -0600

    feat: Add slide-in animation to App component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      animation: {
+        "slide-in": "slide-in 1s ease 500ms forwards",
+      },
+      keyframes: {
+        "slide-in": {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
         },
       },
+    },
   },
   plugins: [],
-}
+};
